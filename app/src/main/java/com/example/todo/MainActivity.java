16f2307d328ec6217b2883b3c3f7fce31d3b4d5d
Fazid Samoon <tuan.fazid@outlook.com>
2022-09-11 02:21:09 +0530

    Insert data function created

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    ListView listView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        listView = findViewById(R.id.listView);
+        textView = findViewById(R.id.textView3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddTodo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
